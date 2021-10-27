2d45e916b9f0fbfbba0bef5808c9e1754d59857a
MeherajMuhammad <joycomputers36@gmail.com>
2021-10-27 19:39:42 +0300

    google sign in done

---
+++ src/hooks/useFirebase.js
@@ -0,0 +1,52 @@
+import initializeAuthentication from '../Firebase/firebase.init'
+import { getAuth, signInWithPopup, GoogleAuthProvider, signOut, onAuthStateChanged } from "firebase/auth";
+import { useState, useEffect } from 'react'
+initializeAuthentication()
+const useFirebase = () => {
+    const auth = getAuth();
+    const googleProvider = new GoogleAuthProvider();
+
+    // user data store
+    const [user, setUser] = useState({})
+    const [error, setError] = useState('')
+
+    // google Login btn
+    const handleGoogleSignIn = () => {
+        signInWithPopup(auth, googleProvider)
+            .then(result => {
+                setUser(result.user)
+            })
+            .catch(error => {
+                setError(error.message)
+            })
+    }
+
+    // signOut btn
+    const handleSignOut = () => {
+        signOut(auth).then(() => {
+            setUser({})
+        })
+            .catch(error => {
+                setError(error.message)
+            })
+    }
+
+    // observer for the user
+    useEffect(() => {
+        onAuthStateChanged(auth, user => {
+            if (user.displayName) {
+                setUser(user)
+            }
+            else {
+                setUser({})
+            }
+        })
+    }, [])
+
+
+    return {
+        user, error, handleGoogleSignIn, handleSignOut
+    }
+}
+
+export default useFirebase;
